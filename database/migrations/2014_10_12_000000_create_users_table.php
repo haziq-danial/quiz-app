1c9ce91966fb23fd8b06dbcabfa13f62682f2b30
Haziq Danial <haziqdanial031@gmail.com>
2022-10-31 22:46:01 +0800

    updated user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id('UserID');
+            $table->string('username');
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->integer('role_type');
+            $table->string('full_name');
+            $table->integer('age');
             $table->rememberToken();
             $table->timestamps();
         });
