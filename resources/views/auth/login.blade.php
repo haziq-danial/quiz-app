14a0da9c09c5b102cc652f65b5e1da2cc3ec2846
haziq-danial <47897951+haziq-danial@users.noreply.github.com>
2023-01-05 21:23:53 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="#"><b>SAINSKU MENTAL HEALTH System</b></a>
+        <a href="#"><b>Quiz Management System</b></a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
