a6a84d464a092155f963b6e7a7b04dfe275fa3f2
haziq-danial <47897951+haziq-danial@users.noreply.github.com>
2023-01-05 21:22:45 +0800

    Update login.blade.php
    edit login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SMHTS System | Log in</title>
+    <title>Quiz Management System | Log in</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
